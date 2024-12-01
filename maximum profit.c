1cbad7b969c1e855edbf59ef2f0677d4b6f492a5
deepika1ojha <134528518+deepika1ojha@users.noreply.github.com>
2024-12-01 20:34:06 +0530

    Create maximum profit.c

---
+++ maximum profit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int maxProfit(int* prices, int size) {
+    if (size <= 1) {
+        return 0; 
+    }
+
+    int minPrice = prices[0]; 
+    int maxProfit = 0;       
+
+    for (int i = 1; i < size; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+        
+        int profit = prices[i] - minPrice;
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int size = sizeof(prices) / sizeof(prices[0]);
+
+    int result = maxProfit(prices, size);
+
+    printf("Maximum Profit: %d\n", result);
+
+    return 0;
+}
